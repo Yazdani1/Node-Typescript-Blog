2ed85a3092b3281ecd46d2e0f2ddcf1a063a7e49
Yazdani Chowdhury <yaz4dani@gmail.com>
2023-09-10 22:54:07 +0300

    WIP

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ const port: number = 3000;
 app.use(cors());
 app.use(express.json({ limit: '4.5mb' }));
 app.use(express.urlencoded({ extended: true }));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
