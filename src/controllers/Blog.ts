0e2724c4211e1e52446974d2e7ef8610e36665f1
Yazdani Chowdhury <yaz4dani@gmail.com>
2023-09-11 22:49:51 +0300

    WIP

--- src/controllers/Blog.ts
+++ src/controllers/Blog.ts
@@ -56,6 +56,7 @@ export const getSingleBlogPost = async (req: Request, res: Response) => {
     const query = { slug: req.params.slug };
 
     const singleBlogPost = await Blog.findOne(query);
+
     if (!singleBlogPost) {
       return res.status(404).json({ error: 'Blog post could not found!' });
     }
