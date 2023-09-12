89a525322e3bdffee5ed588615ab630f52377e32
Yazdani Chowdhury <yaz4dani@gmail.com>
2023-09-12 21:35:50 +0300

    WIP

--- src/controllers/Blog.ts
+++ src/controllers/Blog.ts
@@ -34,6 +34,7 @@ export const createBlog = async (req: Request, res: Response) => {
  * @param req
  * @param res
  */
+
 export const getAllBlogPosts = async (req: Request, res: Response) => {
   try {
     const allBlogPosts: IBlog[] = await Blog.find({}).sort({ date: -1 });
@@ -54,13 +55,10 @@ export const getAllBlogPosts = async (req: Request, res: Response) => {
 export const getSingleBlogPost = async (req: Request, res: Response) => {
   try {
     const query = { slug: req.params.slug };
-
     const singleBlogPost = await Blog.findOne(query);
-
     if (!singleBlogPost) {
       return res.status(404).json({ error: 'Blog post could not found!' });
     }
-
     res.status(200).json(singleBlogPost);
   } catch (error) {
     res.status(500).json({ error: 'Something went wrong' });
